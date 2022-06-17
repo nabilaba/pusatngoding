72a627a27154617c8e855f31694672db08edc793
nabilaba <nabilazizbimaanggita@gmail.com>
2022-06-17 13:35:14 +0700

    feat: Membuat animasi scroll ke atas ketika berpindah halaman

--- Frontend/src/routes/RoutesOutlet.js
+++ Frontend/src/routes/RoutesOutlet.js
@@ -2,13 +2,14 @@ import { Flex, Box } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
-
+import ScrollToTop from "./ScrollToTop"
 
 export default function RoutesOutlet(props) {
   return (
     <Flex minH={"100vh"} justifyContent={"space-between"} direction={"column"}>
       <Navbar {...props} />
       <Box my={"auto"}>
+        <ScrollToTop />
         <Outlet />
       </Box>
       <Footer />
