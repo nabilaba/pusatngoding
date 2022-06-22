16b1093b56d177dc2e8d427da9693571a382e7f6
nabilaba <nabilazizbimaanggita@gmail.com>
2022-06-22 18:58:29 +0700

    fix: api server

--- Frontend/src/api/API.js
+++ Frontend/src/api/API.js
@@ -1,4 +1,4 @@
-exports.BASE_URL = "http://final-project-api-kel77.herokuapp.com";
+exports.BASE_URL = "https://final-project-api-kel77.herokuapp.com";
 exports.ADMIN = exports.BASE_URL + "/admin";
 exports.MENTOR = exports.BASE_URL + "/mentor";
 exports.SISWA = exports.BASE_URL + "/siswa";
