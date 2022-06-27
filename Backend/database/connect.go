cfaf73313f1efb1abba274195fe00d83642d17a2
seanlim12 <seansebastian27@gamil.com>
2022-06-27 18:50:21 +0700

    FEAT: add connect

---
+++ Backend/database/connect.go
@@ -0,0 +1,17 @@
+package database
+
+//import "backend/model"
+
+//var DB
+
+func Connect() {
+	// connection, err :=
+
+	// if err != nil {
+	// 	panic("database tidak terhubung")
+	// }
+
+	//DB = connect
+
+	//connect.AutoMigrate(&model.User{})
+}
