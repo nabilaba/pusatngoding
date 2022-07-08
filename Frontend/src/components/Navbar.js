ded9c220c840daab28e8f03d660ee7a383d1c480
nabilaba <nabilazizbimaanggita@gmail.com>
2022-07-08 20:23:15 +0700

    fix: Responsive Navbar

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -46,6 +46,7 @@ const NavLink = ({ nama, link, onClick }) => (
       textDecoration: "none",
     }}
     onClick={onClick}
+    w="max-content"
   >
     {nama}
   </Text>
@@ -101,163 +102,160 @@ export default function Navbar() {
   }, [getUser]);
 
   return (
-    <>
-      <Box
-        as="header"
-        position={"sticky"}
-        top={0}
-        left={0}
-        right={0}
-        w={"full"}
-        zIndex={3}
-        bg={bgnavbar}
-        backdropFilter="auto"
-        backdropsaturation="180%"
-        backdropBlur="5px"
-        boxShadow="sm"
-      >
-        <Container maxW={"7xl"}>
-          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-            <IconButton
-              variant="ghost"
-              size={"md"}
-              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-              aria-label={"Open Menu"}
-              display={{ md: "none" }}
-              onClick={isOpen ? onClose : onOpen}
-            />
-            <HStack spacing={8} alignItems={"center"}>
-              <Box>
-                <HStack as={LinkTo} to="/" alignItems={"center"}>
-                  <Image src={Logo} alt="" />
-                  <Heading
-                    as="h3"
-                    size="md"
-                    fontWeight={500}
-                    display={{ base: "none", sm: "block" }}
+    <Box
+      as="header"
+      position={"sticky"}
+      top={0}
+      left={0}
+      right={0}
+      w={"full"}
+      zIndex={3}
+      bg={bgnavbar}
+      backdropFilter="auto"
+      backdropsaturation="180%"
+      backdropBlur="5px"
+      boxShadow="sm"
+    >
+      <Container maxW={"7xl"}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            variant="ghost"
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>
+              <HStack as={LinkTo} to="/" alignItems={"center"}>
+                <Image src={Logo} alt="" />
+                <Heading
+                  as="h3"
+                  size="md"
+                  fontWeight={500}
+                  display={{ base: "none", sm: "block" }}
+                >
+                  <Text
+                    color={useColorModeValue(
+                      "accentLight.400",
+                      "accentDark.400"
+                    )}
+                    as={"span"}
+                    position={"relative"}
+                    _after={{
+                      content: "''",
+                      width: "full",
+                      height: "15%",
+                      position: "absolute",
+                      bottom: 1,
+                      left: 0,
+                      bg: useColorModeValue(
+                        "accentLight.100",
+                        "accentDark.900"
+                      ),
+                      zIndex: -1,
+                    }}
                   >
-                    <Text
-                      color={useColorModeValue(
-                        "accentLight.400",
-                        "accentDark.400"
-                      )}
-                      as={"span"}
-                      position={"relative"}
-                      _after={{
-                        content: "''",
-                        width: "full",
-                        height: "15%",
-                        position: "absolute",
-                        bottom: 1,
-                        left: 0,
-                        bg: useColorModeValue(
-                          "accentLight.100",
-                          "accentDark.900"
-                        ),
-                        zIndex: -1,
-                      }}
-                    >
-                      pusat
-                    </Text>
-                    <br />
-                    ngoding
-                  </Heading>
-                </HStack>
-              </Box>
-            </HStack>
-            <Flex alignItems={"center"}>
-              <HStack
-                as={"nav"}
-                spacing={4}
-                display={{ base: "none", md: "flex" }}
-                mr={4}
-              >
-                {isLoggedIn ? (
-                  <NavLink
-                    nama="Dashboard"
-                    link="dashboard"
-                    onClick={onClose}
-                  />
-                ) : null}
-                {Links.map((link) => (
-                  <NavLink key={link.nama} {...link} />
-                ))}
+                    pusat
+                  </Text>
+                  <br />
+                  ngoding
+                </Heading>
               </HStack>
-              <IconButton
-                variant={"ghost"}
-                size={"md"}
-                icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
-                mr={4}
-                onClick={toggleColorMode}
-              />
-              {isLoggedIn ? (
-                <Flex alignItems={"center"}>
-                  <Menu>
-                    <MenuButton
-                      as={Button}
-                      rounded={"full"}
-                      variant={"link"}
-                      cursor={"pointer"}
-                      minW={0}
-                    >
-                      <Avatar
-                        size={"sm"}
-                        name={`${user.nama_depan} ${user.nama_belakang}`}
-                        src={user.avatar}
-                      />
-                    </MenuButton>
-                    <MenuList>
-                      {loggedAs === "admin" ? null : (
-                        <MenuItem
-                          icon={<EditIcon />}
-                          as={LinkTo}
-                          to="dashboard/akun"
-                        >
-                          Akun Saya
-                        </MenuItem>
-                      )}
-                      <MenuItem
-                        as={LinkTo}
-                        to={"dashboard/transaksi"}
-                        icon={<RepeatClockIcon />}
-                      >
-                        Riwayat Transaksi
-                      </MenuItem>
-                      <MenuItem
-                        icon={<ArrowBackIcon />}
-                        onClick={() => HandleLogOut()}
-                      >
-                        Keluar
-                      </MenuItem>
-                    </MenuList>
-                  </Menu>
-                </Flex>
-              ) : (
-                <Button as={LinkTo} to="/masuk" size={"sm"} {...navbarSet}>
-                  MASUK
-                </Button>
-              )}
-            </Flex>
-          </Flex>
-
-          {isOpen ? (
-            <Box pb={4} display={{ md: "none" }}>
-              <Stack as={"nav"} spacing={4}>
-                {isLoggedIn ? (
-                  <NavLink
-                    nama="Dashboard"
-                    link="dashboard"
-                    onClick={onClose}
-                  />
-                ) : null}
-                {Links.map((link) => (
-                  <NavLink key={link.nama} {...link} onClick={onClose} />
-                ))}
-              </Stack>
             </Box>
-          ) : null}
+          </HStack>
+          <Flex alignItems={"center"}>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+              mr={4}
+            >
+              {isLoggedIn ? (
+                <NavLink nama="Dashboard" link="dashboard" onClick={onClose} />
+              ) : null}
+              {Links.map((link) => (
+                <NavLink key={link.nama} {...link} />
+              ))}
+            </HStack>
+            <IconButton
+              variant={"ghost"}
+              size={"md"}
+              icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              mr={4}
+              onClick={toggleColorMode}
+            />
+            {isLoggedIn ? (
+              <Flex alignItems={"center"}>
+                <Menu>
+                  <MenuButton
+                    as={Button}
+                    rounded={"full"}
+                    variant={"link"}
+                    cursor={"pointer"}
+                    minW={0}
+                  >
+                    <Avatar
+                      size={"sm"}
+                      name={`${user.nama_depan} ${user.nama_belakang}`}
+                      src={user.avatar}
+                    />
+                  </MenuButton>
+                  <MenuList>
+                    {loggedAs === "admin" ? null : (
+                      <MenuItem
+                        icon={<EditIcon />}
+                        as={LinkTo}
+                        to="dashboard/akun"
+                      >
+                        Akun Saya
+                      </MenuItem>
+                    )}
+                    <MenuItem
+                      as={LinkTo}
+                      to={"dashboard/transaksi"}
+                      icon={<RepeatClockIcon />}
+                    >
+                      Riwayat Transaksi
+                    </MenuItem>
+                    <MenuItem
+                      icon={<ArrowBackIcon />}
+                      onClick={() => HandleLogOut()}
+                    >
+                      Keluar
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Flex>
+            ) : (
+              <Button as={LinkTo} to="/masuk" size={"sm"} {...navbarSet}>
+                MASUK
+              </Button>
+            )}
+          </Flex>
+        </Flex>
+      </Container>
+      {isOpen ? (
+        <Container
+          display={{ md: "none" }}
+          pos="fixed"
+          zIndex={3}
+          bg={bgnavbar}
+          w={"full"}
+          maxW="7xl"
+          p={4}
+        >
+          <Stack as={"nav"} spacing={4}>
+            {isLoggedIn ? (
+              <NavLink nama="Dashboard" link="dashboard" onClick={onClose} />
+            ) : null}
+            {Links.map((link) => (
+              <NavLink key={link.nama} {...link} onClick={onClose} />
+            ))}
+          </Stack>
         </Container>
-      </Box>
-    </>
+      ) : null}
+    </Box>
   );
 }
